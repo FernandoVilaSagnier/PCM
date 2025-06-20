57d6b194f3af6d742d7580f7cf2f7ddf7c7b61f9
Fernando Vilá <fernandovila@iMacdeFernando.home>
2025-06-20 10:42:35 +0200

    Añadido PDF de roles de usuario

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -184,8 +184,7 @@ export default function HomePage() {
         href="/roles3.pdf"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-sm px-4 py-2 border rounded-xl border-gray-300 hover:bg-gray-100 mr-2 mb-2 inline-block text-center"
-      >
+        className="inline-flex items-center justify-center rounded-xl border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-100"      >
         {title}
       </a>
     );
